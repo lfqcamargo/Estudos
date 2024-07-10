6b4abf67abac43eea1c94f74840e3c407ada36d4
Lucas Camargo <lfqcamargo@gmail.com>
2024-07-10 20:30:03 -0300

    Adicionado banco de dados em formato json

--- Node/api-sem-biblioteca/src/server.js
+++ Node/api-sem-biblioteca/src/server.js
@@ -1,23 +1,32 @@
 import http from 'node:http'
-import { json } from './middlewares/json.js'
+import { randomUUID } from 'node:crypto'
 
-const users = []
+import { json } from './middlewares/json.js'
+import { Database } from './database.js'
+
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
-  if (method === 'GET' && url === '/users') return res.end(JSON.stringify(users))
+  if (method === 'GET' && url === '/users') {
+    const users = database.select('users')
+
+    return res.end(JSON.stringify(users))
+  }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
 
-    users.push({
-      id: 1,
+    const user = {
+      id: randomUUID(),
       name,
       email,
-    })
+    }
+
+    database.insert('users', user)
 
     return res.writeHead(201).end()
   }
