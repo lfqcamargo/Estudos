7113d0b66a9f66282397a824fa25552bb317ee39
Lucas Camargo <lfqcamargo@gmail.com>
2024-07-16 20:48:17 -0300

    Adicionando Modal com react-dialog

--- React/dt-money/src/components/Header/index.tsx
+++ React/dt-money/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/Logo.svg'
 
@@ -8,7 +9,19 @@ export function Header() {
       <HeaderContent>
         <img src={logoImg} alt="" />
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>          
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+              <Dialog.Close/>
+            </Dialog.Content>
+          </Dialog.Portal>  
+        </Dialog.Root>      
       </HeaderContent>
     </HeaderContainer>
   )
