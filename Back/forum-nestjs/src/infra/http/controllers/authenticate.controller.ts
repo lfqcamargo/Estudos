161292db9e08e03a37cf30beaae3cfe08ff75896
lfqcamargo <lfqcamargo@gmail.com>
2024-12-30 15:06:26 -0300

    Update

--- Back/forum-nestjs/src/infra/http/controllers/authenticate.controller.ts
+++ Back/forum-nestjs/src/infra/http/controllers/authenticate.controller.ts
@@ -22,7 +22,7 @@ type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>
 @Controller('/sessions')
 @Public()
 export class AuthenticateController {
-  constructor(private authenticateStudent: AuthenticateStudentUseCase) {}
+  constructor(private authenticateStudent: AuthenticateStudentUseCase) { }
 
   @Post()
   @UsePipes(new ZodValidationPipe(authenticateBodySchema))
